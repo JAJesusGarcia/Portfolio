93b6b7103c45fcca88550af4487197bc73750aac
JAJesusGarcia <jesusjagarcia98@gmail.com>
2025-01-18 08:30:23 -0300

    nodemailer

---
+++ back/config/nodemailes.ts
@@ -0,0 +1,18 @@
+import nodemailer from 'nodemailer';
+
+export const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export const mailOptions = (name: string, email: string, message: string) => ({
+  from: process.env.EMAIL_USER,
+  to: process.env.EMAIL_RECEIVER,
+  subjet: `Nuevo mensaje de ${name}`,
+  html: `<p><strong>Nombre:</strong> ${name}</p>
+         <p><strong>Email:</strong> ${email}</p>
+         <p><strong>Mensaje:</strong> ${message}</p>`,
+});
