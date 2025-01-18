bd73705cccb8c95fbcefb1b9db5ee51ea0e86ebc
JAJesusGarcia <jesusjagarcia98@gmail.com>
2025-01-18 11:36:33 -0300

    currentYear

--- portfolio/src/components/Footer/Footer.tsx
+++ portfolio/src/components/Footer/Footer.tsx
@@ -1,4 +1,6 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="h-[110px] flex-1 border-t bg-secondary p-5 text-quinary">
       <div className="container my-5 text-center">
@@ -6,7 +8,7 @@ const Footer = () => {
           My Portfolio
         </h4>
         <p className="text-tertiary">
-          <span className="text-primary/50">©</span> 2024 Jesús Garcia{" "}
+          <span className="text-primary/50">©</span> {currentYear} Jesús Garcia{" "}
           <span className="text-primary/50">|</span> All rights reserved.{" "}
         </p>
       </div>
