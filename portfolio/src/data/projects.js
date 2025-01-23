17f02e4bb4064327af31f4943cfba10b9ee39eaf
JAJesusGarcia <jesusjagarcia98@gmail.com>
2025-01-23 13:57:24 -0300

    add proyect

--- portfolio/src/data/projects.js
+++ portfolio/src/data/projects.js
@@ -1,9 +1,27 @@
 export const projects = [
   {
+    title: "AI-technical-test",
+    description:
+      "Este proyecto implementa un frontend avanzado para análisis de imágenes histológicas de cáncer de mama y próstata, utilizando Next.js y React.",
+    image: "/imge/IA-TECHNICA.webp",
+    url: "https://ai-technical-test.vercel.app/",
+    githubUrl: "https://github.com/JAJesusGarcia/AI-technical-test",
+    technologies: [
+      "Next.js",
+      "React",
+      "Tailwind CSS",
+      "UX/UI Fundamentals",
+      "SEO",
+      "SHADCN/UI",
+      "Auth",
+      "Vercel",
+    ],
+  },
+  {
     title: "E-commerce",
     description:
       "Plataforma E-commerce, con consignas en base a la rama de especialización (FrontEnd)",
-    image: "/imge/m4.png",
+    image: "/imge/m4.webp",
     url: "https://yisus-shop.vercel.app/",
     githubUrl: "https://github.com/JAJesusGarcia/YisusShop",
     technologies: [
@@ -20,7 +38,7 @@ export const projects = [
     title: "Synergy2Devs",
     description:
       "Transformamos ideas en realidades innovadoras. Creamos sitios web dinámicos y soluciones digitales personalizadas que impulsan el crecimiento y la visibilidad de tu emprendimiento.",
-    image: "/imge/synergy2.png",
+    image: "/imge/synergy2.webp",
     url: "https://www.synergy2devs.com/proyectos",
     githubUrl: "https://github.com/Synergy2Devs/website",
     technologies: ["Next.js", "React", "Full Stack"],
@@ -29,7 +47,7 @@ export const projects = [
     title: "NetepScript",
     description:
       "Este script instala Node, Express, TypeScript, ESLint y Prettier automáticamente. La idea detrás de este script es ejecutar todas las instalaciones de dependencias y configuraciones de entorno con un solo comando, agilizando la configuración del proyecto desde cero.",
-    image: "/imge/netep.png",
+    image: "/imge/netep.webp",
     url: "https://www.npmjs.com/package/netepscript",
     githubUrl: "https://github.com/Synergy2Devs/netepScript",
     technologies: ["Bash", "NPM"],
@@ -38,7 +56,7 @@ export const projects = [
     title: "Eventos Sociales",
     description:
       "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.",
-    image: "/imge/aledj.png",
+    image: "/imge/aledj.webp",
     url: "https://alefuocoeventos.com/",
     githubUrl: "https://github.com/Synergy2Devs/website",
     technologies: ["Next.js", "React", "Full Stack"],
@@ -47,7 +65,7 @@ export const projects = [
     title: "Gestor de Turnos",
     description:
       "Sistema de gestión de Turnos para los clientes de un servicio... Termina de conformar el Stack base del perfil de un Full Stack",
-    image: "/imge/m3.png",
+    image: "/imge/m3.webp",
     url: "https://synergy2-devs.vercel.app/home",
     githubUrl: "https://github.com/JAJesusGarcia/Synergy2Devs",
     technologies: [
@@ -64,7 +82,7 @@ export const projects = [
     title: "Cinema - JessFlix",
     description:
       "Sistema de gestión de peliculas. CRUD completo para el modelado de datos y su gestión a travez de la interacción con el usuario en su interfaz",
-    image: "/imge/m2.png",
+    image: "/imge/m2.webp",
     url: "https://jajesusgarcia.github.io/JessFlix/",
     githubUrl: "https://github.com/jajesusgarcia/JessFlix",
     technologies: [
@@ -81,7 +99,7 @@ export const projects = [
     title: "My Web Page",
     description:
       "Pagina Web de presentación personal y actividades favoritas. Interacción con el usuario para la integración de los 3 pilares del Desarrollo Web",
-    image: "/imge/m1.png",
+    image: "/imge/m1.webp",
     url: "https://jajesusgarcia.github.io/Primer-Pagina-Web-con-JS/",
     githubUrl: "https://github.com/jajesusgarcia/Primer-Pagina-Web-con-JS",
     technologies: ["HTML", "CSS", "JavaScript"],
